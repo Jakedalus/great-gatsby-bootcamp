deacdc42dd2b7ade08e07b001704739438b2fc01
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-10-26 18:25:06 -0400

    Add about and contact pages

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AboutPage = () => (
+	<div>
+		<h1>About</h1>
+		<p>I was born and now I do things</p>
+	</div>
+);
+
+export default AboutPage;
---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactPage = () => (
+	<div>
+		<h1>Contact Me</h1>
+		<p>jacquedalus@gmail.com</p>
+		<p>jacob-carpenter.com</p>
+	</div>
+);
+
+export default ContactPage;
