c21491891ef36717ffe00b5a97400f9d371853ef
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-11-05 18:38:09 -0500

    Add remark and display md files on Blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,42 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import { graphql, useStaticQuery } from 'gatsby';
 
-const BlogPage = () => (
-	<Layout>
-		<h1>Blog</h1>
-		<p>Posts will show up here later on</p>
-	</Layout>
-);
+const BlogPage = () => {
+	const data = useStaticQuery(graphql`
+		{
+			allMarkdownRemark {
+				edges {
+					node {
+						frontmatter {
+							title
+							date
+						}
+						html
+						excerpt
+						timeToRead
+					}
+				}
+			}
+		}
+	`);
+
+	console.log(data);
+
+	return (
+		<Layout>
+			<h1>Blog</h1>
+			<p>Posts will show up here later on</p>
+			<ol>
+				{data.allMarkdownRemark.edges.map(post => (
+					<li>
+						<h2>{post.node.frontmatter.title}</h2>
+						<p>{post.node.frontmatter.date}</p>
+					</li>
+				))}
+			</ol>
+		</Layout>
+	);
+};
 
 export default BlogPage;
