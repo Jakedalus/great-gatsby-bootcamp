bd784b400210f1b132dfd3868adeedf1af96f061
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-11-11 17:20:41 -0500

    Add createPages to create pages for blog posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,4 @@
+const { graphql } = require('gatsby');
 const path = require('path');
 
 module.exports.onCreateNode = ({ node, actions }) => {
@@ -20,4 +21,37 @@ module.exports.onCreateNode = ({ node, actions }) => {
 			value : slug
 		});
 	}
+};
+
+module.exports.createPages = async ({
+	graphql,
+	actions
+}) => {
+	const { createPage } = actions;
+	const blogTemplate = path.resolve(
+		'./src/templates/blog.js'
+	);
+
+	const response = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }`);
+
+	response.data.allMarkdownRemark.edges.forEach(edge => {
+		createPage({
+			component : blogTemplate,
+			path      : `/blog/${edge.node.fields.slug}`,
+			context   : {
+				slug : edge.node.fields.slug
+			}
+		});
+	});
 };
